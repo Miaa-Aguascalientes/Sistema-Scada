44287a703c47581cd717b9563f8a31d8ad47bb84
Miaa <pedroguillermomiaa@gmail.com>
2026-04-21 14:01:19 -0600

    Update Miaa Scada.py

--- Miaa Scada.py
+++ Miaa Scada.py
@@ -835,7 +835,7 @@ if sector_seleccionado:
             /* 4. Contenedor de indicadores pegado al título */
             .metrics-row {{
                 margin-top: 0px !important;
-                margin-bottom: 0px !important;
+                margin-bottom: -10px !important;
             }}
 
             .micro-card {{
