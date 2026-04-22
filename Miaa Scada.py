c8529855b7b92efd52a0b263011e988f5ea94b1f
Miaa <pedroguillermomiaa@gmail.com>
2026-04-22 08:12:37 -0600

    Update Miaa Scada.py

--- Miaa Scada.py
+++ Miaa Scada.py
@@ -888,7 +888,7 @@ if sector_seleccionado:
         with c_sel1:
             opcion_fecha = st.selectbox("Rango:", ["Hoy", "Esta Semana", "Últimos 14 días", "Este Mes", "Personalizado"], index=2, key="f_sector_full")
         with c_sel2:
-            sel_r = st.selectbox("Equipo:", list(reg_nombres.keys()), key="sel_reg_full")
+            sel_r = st.selectbox("Registrador:", list(reg_nombres.keys()), key="sel_reg_full")
 
         # 7.4. Layout: Mapa e Histórico
         col_izq, col_der = st.columns([1.1, 0.9])
