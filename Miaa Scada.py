3856f4ea58a6158d824f568519f454d32a541ef0
Miaa <pedroguillermomiaa@gmail.com>
2026-04-27 14:08:23 -0600

    Update Miaa Scada.py

--- Miaa Scada.py
+++ Miaa Scada.py
@@ -892,7 +892,7 @@ if sector_seleccionado:
             .micro-value {{ color: #ffffff; font-size: 16px; font-weight: bold; }}
             
             .col-mapa-offset {{
-                margin-top: 10px !important; 
+                margin-top: 100px !important; 
             }}
 
             hr {{
