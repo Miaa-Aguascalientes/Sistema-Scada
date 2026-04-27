cbbd4af646717aede1085d3ede25ee3157413f4d
Miaa <pedroguillermomiaa@gmail.com>
2026-04-27 11:09:43 -0600

    Update Miaa Scada.py

--- Miaa Scada.py
+++ Miaa Scada.py
@@ -643,7 +643,7 @@ st.markdown("""
         /* CONTENEDOR DE INDICADORES (HUD FIJO) */
         .contenedor-indicadores {
            position: fixed;
-           top: 45px; 
+           top: 65px; 
            left: 320px;
            right: 0;
            display: flex;
