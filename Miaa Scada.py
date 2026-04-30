510c3e9bb53a917f8d9b6cc2be84ea9302a9dce5
Miaa <pedroguillermomiaa@gmail.com>
2026-04-30 11:20:08 -0600

    Update Miaa Scada.py

--- Miaa Scada.py
+++ Miaa Scada.py
@@ -1336,19 +1336,12 @@ if sector_seleccionado:
                        </style>
                        """, unsafe_allow_html=True)                        
 
-# 1. El Título (con margen superior negativo para acercarse al mapa)
-                    st.markdown(f"""
-                        <div style='margin-top: -50px; margin-bottom: 0px;'>
-                        <h3 style='color:#00ffcc; font-size:16px; font-family:sans-serif;'>
-                            Histórico VRP: {sel_vrp}
-                        </h3>
-                    </div>
-                    """, unsafe_allow_html=True)
+
 
                     fig_vrp.update_layout(
                         paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', 
                         height=280, 
-                        margin=dict(l=50, r=50, t=0, b=10),
+                        margin=dict(l=50, r=50, t=-50, b=10),
                         hovermode="x unified",
                         legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="left", x=0, font=dict(color="white", size=9)),
                         xaxis=dict(showgrid=True, gridcolor='rgba(255, 255, 255, 0.05)', color="white"),
