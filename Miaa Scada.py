434d2332cc99a7855773535964c985000d0e62d2
Miaa <pedroguillermomiaa@gmail.com>
2026-04-30 09:16:33 -0600

    Update Miaa Scada.py

--- Miaa Scada.py
+++ Miaa Scada.py
@@ -998,7 +998,7 @@ if sector_seleccionado:
         dict_vrp = {k: v for k, v in dict_vrp_all.items() if str(v.get('sector')).strip() == str(sec_id).strip()}
         vrp_nombres = {v['nombre']: k for k, v in dict_vrp.items()}
         
-        c_vacia, c_sel1, c_sel2 = st.columns([1.0, 150.00, 150.00, 2])
+        c_vacia, c_sel1, c_sel2, c_sel3 = st.columns([1.0, 150.00, 150.00, 2])
         with c_sel1:
             opcion_fecha = st.selectbox("Rango de fechas:", ["Hoy", "Esta Semana", "Últimos 14 días", "Este Mes", "Personalizado"], index=2, key="f_sector_full")
         with c_sel1:
