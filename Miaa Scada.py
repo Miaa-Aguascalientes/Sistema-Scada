963595ae99d57e3ad5ad196f97d261e1d346f8dc
Miaa <pedroguillermomiaa@gmail.com>
2026-04-30 11:36:18 -0600

    Update Miaa Scada.py

--- Miaa Scada.py
+++ Miaa Scada.py
@@ -1333,7 +1333,7 @@ if sector_seleccionado:
                     fig_vrp.update_layout(
                         paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', 
                         height=280, 
-                        margin=dict(l=50, r=50, t=100, b=10),
+                        margin=dict(l=50, r=50, t=0, b=100),
                         hovermode="x unified",
                         legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="left", x=0, font=dict(color="white", size=9)),
                         xaxis=dict(showgrid=True, gridcolor='rgba(255, 255, 255, 0.05)', color="white"),
