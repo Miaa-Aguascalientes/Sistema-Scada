1edec9c3504748082ce0b72b5e8d1461a919eac3
Miaa <pedroguillermomiaa@gmail.com>
2026-05-05 11:37:55 -0600

    Update Miaa Scada.py

--- Miaa Scada.py
+++ Miaa Scada.py
@@ -1986,4 +1986,4 @@ if sectores_data:
                 MousePosition().add_to(m_sec)
            
     folium.LayerControl(position='topright', collapsed=False).add_to(m)          
-    folium_static(m, width=1600, height=750)
+    folium_static(m, width=None, height=750)
